f0932f1bc6ba473cdb642289e72e1b4cdc409bb8
victus <azamatkhakimov0@gmail.com>
2025-05-25 11:29:47 +0300

    Rabochiy kod

--- keyboard.py
+++ keyboard.py
@@ -14,7 +14,7 @@ def start_buttons(lang):
          KeyboardButton(text=f"{messages[lang]['branches']}")],
         [KeyboardButton(text=f"{messages[lang]['job_positions']}")],
         [KeyboardButton(text=f"{messages[lang]['menu']}",
-                        web_app=WebAppInfo(url="https://bunyodnaimov.github.io/p2-evos-test-bot/")),
+                        web_app=WebAppInfo(url="https://muhammadayub-nah-id-win.github.io/evosp2_bot/")),
          KeyboardButton(text=f"{messages[lang]['news']}")],
         [
             KeyboardButton(text=f"{messages[lang]['contacts']}"),
