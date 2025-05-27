c01e185dc3bcba8b5a54a381ea1b57407e39d658
victus <azamatkhakimov0@gmail.com>
2025-05-27 10:10:01 +0300

    Rabochiy kod

--- handlears.py
+++ handlears.py
@@ -1,5 +1,7 @@
+import os
+
 from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery, LabeledPrice
+from aiogram.types import Message, CallbackQuery, LabeledPrice, FSInputFile
 from database import database
 from keyboard import select_language, start_buttons
 from messages import messages, job_info
@@ -21,3 +23,20 @@ async def set_language(callback_query: CallbackQuery):
 async def get_jobs(message: Message):
     lang = database.get_user_lang(message.from_user.id)
     await message.answer(text=job_info[lang]["job_position"])
+
+
+@router.message(F.text.in_(["🏢 О кампании", "🏢 Kampaniya haqida"]))
+async def about_company_handlears(message: Message):
+    img = FSInputFile(os.path.join(os.path.dirname(__file__), "images", "evos.png"))
+    text= ("Kompaniyamizning birinchi filiali 2006 yilda ochilgan bo’lib\n"
+           "shu kungacha muvaffaqiyatli faoliyat yuritib kelmoqdaligini bilarmidingiz?\n"
+           "15 yil davomida kompaniya avtobus bekatidagi kichik ovqatlanish joyidan zamonaviy\n"
+           "kengaytirilgan tarmoqqa aylandi, u bugungi kunda O‘zbekiston bo‘ylab 65 dan ortiq restoranlarni\n"
+           "o‘zining eng tezkor yetkazib berish xizmatini, zamonaviy IT-infratuzilmasini va 2000 dan ortiq xodimlarni o‘z ichiga oladi.!")
+
+
+@router.message(F.text == "⬅️Ortga")
+async def ortga(message: Message):
+    text = "Siz bosh sahifaga qayttingiz!"
+    lang = database.get_user_lang(message.from_user.id)
+    await message.answer(text=text, reply_markup=start_buttons(lang))
